b22d598b895c9979cb5fc97a381e3db79e470c18
kasumi <kasumi@kasumi-owari.moe>
2016-03-23 16:38:01 +0100

    Added intersections, music, slowly slowing down if needed to stop

--- src/robot.c
+++ src/robot.c
@@ -14,11 +14,21 @@ const int kMaxSizeOfMessage = 30;
 const int INBOX = 5;
 
 bool stopped;
+bool objectStopped;
+bool commandStopped;
 float deltaTime;
 
-#define MOTOR_SPEED 40
+int direction;
+
+#define MOTOR_SPEED 55
 #define MOTOR_SPEED_TURN 100
 #define MOTOR_SPEED_TURN_NEGATIVE -10
+
+#define STOP_SECONDS 1.0
+
+#define INT_STRAIGHT 1
+#define INT_LEFT 2
+#define INT_RIGHT 3
 
 bool onLine(int color)
 {
@@ -50,7 +60,21 @@ task commands()
     	nRcvBuffer[nSizeOfMessage] = '\0';
     	string s = "";
     	stringFromChars(s, (char *) nRcvBuffer);
-    	nxtDisplayTextLine(1, "%s", s);
+    	nxtDisplayTextLine(2, "%s", s);
+
+    	if (s == "FIRE") {
+    		nxtDisplayTextLine(3, "going");
+    		commandStopped = false;
+    	} else if (s == "DOWN") {
+    		nxtDisplayTextLine(3, "stop");
+    		commandStopped = true;
+    	} else if (s == "LEFT") {
+    		direction = INT_LEFT;
+    	} else if (s == "UP") {
+    		direction = INT_STRAIGHT;
+    	} else if (s == "RIGHT") {
+    		direction = INT_RIGHT;
+    	}
     }
     wait1Msec(100);
 
@@ -64,7 +88,45 @@ task DrivingSound()
 	while(1)
 	{
 		if (!stopped) {
-			playSound(soundFastUpwardTones);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(929, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 83); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 14); while(bSoundActive && !stopped);
+			PlayTone(1841, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 41); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 14); while(bSoundActive && !stopped);
+			PlayTone(1841, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 41); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 55); while(bSoundActive && !stopped);
+			wait1Msec(280);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(695, 14); while(bSoundActive && !stopped);
+			PlayTone(929, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 83); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 14); while(bSoundActive && !stopped);
+			PlayTone(1841, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 41); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 14); while(bSoundActive && !stopped);
+			PlayTone(1841, 83); while(bSoundActive && !stopped);
+			PlayTone(1401, 41); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1188, 14); while(bSoundActive && !stopped);
+			PlayTone(1251, 14); while(bSoundActive && !stopped);
+			PlayTone(1054, 55); while(bSoundActive && !stopped);
 		} else {
 			clearSounds();
 		}
@@ -76,37 +138,93 @@ bool found;
 void LineFolower()
 {
 	found = false;
-	int oldSystime, newSystime;
-	oldSystime = nSysTime;
+
+	int old_system_time = nSysTime;
+	int new_system_time;
+	float deltaTime;
+	float stopped_time = 0.0;
+
+	int rl, rr;
 
 	while (1)
 	{
-		while (stopped)
-		{
-			motor[motor_left] = 0;
-			motor[motor_right] = 0;
+		if (objectStopped || commandStopped) {
+			stopped = true;
+		} else {
+			stopped = false;
 		}
 
-		newSystime = nSysTime;
-		deltaTime = (newSystime - oldSystime) / 1000.0;
-		oldSystime = newSystime;
+		new_system_time = nSysTime;
+		deltaTime = (new_system_time - old_system_time) / 1000.0;
+		old_system_time = new_system_time;
+
+		if (stopped) {
+			stopped_time += deltaTime;
+		} else {
+			stopped_time = 0.0;
+		}
+		if (stopped_time > STOP_SECONDS) {
+			stopped_time = STOP_SECONDS;
+		}
+
+		nxtDisplayTextLine(7, "%f", stopped_time);
+
+		/*while (stopped)
+		{
+			int ml, mr;
+			ml = motor[motor_left];
+			mr = motor[motor_right];
+
+			ml -= rl;
+			mr -= rr;
+			if (ml < 0) ml = 0;
+			if (mr < 0) mr = 0;
+
+			nxtDisplayTextLine(6, "%i = %i", rl, rr);
+
+			motor[motor_left] = ml;
+			motor[motor_right] = mr;
+
+			if (objectStopped || commandStopped) {
+			stopped = true;
+			} else {
+				stopped = false;
+			}
+
+			wait1Msec(250);
+		}*/
 
 		nxtDisplayTextLine(0, "Start");
 		int c = SensorValue[line];
 		nxtDisplayTextLine(3, "Color: %f", c);
 		if (onLine(c)) {
-			motor[motor_left] = MOTOR_SPEED;
-			motor[motor_right] = MOTOR_SPEED;
+			motor[motor_left] = MOTOR_SPEED - (stopped_time * (MOTOR_SPEED / STOP_SECONDS));
+			motor[motor_right] = MOTOR_SPEED - (stopped_time * (MOTOR_SPEED / STOP_SECONDS));
 			found = false;
+			int d = SensorValue[line_check];
+			if (d < 44) {
+				nxtDisplayTextLine(6, "Intersection");
+				if (direction == INT_STRAIGHT) {
+
+				} else if (direction == INT_LEFT) {
+					motor[motor_left] = MOTOR_SPEED_TURN_NEGATIVE;
+					motor[motor_right] = MOTOR_SPEED_TURN;
+					wait1Msec(1800);
+				} else {
+					motor[motor_left] = MOTOR_SPEED_TURN;
+					motor[motor_right] = MOTOR_SPEED_TURN_NEGATIVE;
+					wait1Msec(1800);
+				}
+			}
 		} else {
 			int d = SensorValue[line_check];
 			if (d < 44 || found) {
-				motor[motor_left] = MOTOR_SPEED_TURN_NEGATIVE;
-				motor[motor_right] = MOTOR_SPEED_TURN;
+				motor[motor_left] = MOTOR_SPEED_TURN_NEGATIVE - (stopped_time * (MOTOR_SPEED_TURN_NEGATIVE / STOP_SECONDS));
+				motor[motor_right] = MOTOR_SPEED_TURN - (stopped_time * (MOTOR_SPEED_TURN / STOP_SECONDS));
 				found = true;
 			} else {
-				motor[motor_left] = MOTOR_SPEED_TURN;
-				motor[motor_right] = MOTOR_SPEED_TURN_NEGATIVE;
+				motor[motor_left] = MOTOR_SPEED_TURN - (stopped_time * (MOTOR_SPEED_TURN / STOP_SECONDS));
+				motor[motor_right] = MOTOR_SPEED_TURN_NEGATIVE - (stopped_time * (MOTOR_SPEED_TURN_NEGATIVE / STOP_SECONDS));
 			}
 		}
 
@@ -120,10 +238,10 @@ task ObjectInWay()
 	{
 		int distance = SensorValue[sonar];
 		if (distance < 25) {
-			stopped = true;
+			objectStopped = true;
 			nxtDisplayTextLine(1, "Object in way %f", distance);
 		} else {
-			stopped = false;
+			objectStopped = false;
 			nxtDisplayTextLine(1, "No object %f", distance);
 		}
 
@@ -134,6 +252,9 @@ task ObjectInWay()
 task main()
 {
 	stopped = false;
+	objectStopped = false;
+	commandStopped = false;
+	direction = INT_STRAIGHT;
 
 	startTask(commands);
 	startTask(ObjectInWay);
